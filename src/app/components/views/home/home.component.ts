eef14c94b34b0dd8952e4b7495a01fb2130e1009
Bejo62192 <130389920+Bejo62192@users.noreply.github.com>
2023-05-23 08:46:38 +0200

    Ajout methode getAllAnnoucement

--- src/app/components/views/home/home.component.ts
+++ src/app/components/views/home/home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AnnouncementModel } from 'src/app/models/announcement.model';
+import { ApiService } from 'src/app/services/api.service';
+import { SnackbarService } from 'src/app/services/snackbar.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  public announcements?: AnnouncementModel[];
+
+  constructor(
+    private readonly _api: ApiService,
+    private readonly _snackbar: SnackbarService
+  ) { }
+
+  ngOnInit(): void {
+    this.getAllAnnouncement();
+  }
+  public async getAllAnnouncement() {
+    try {
+      this.announcements = await this._api.get("/announcement/home");
+    } catch (e) {
+      console.error(e);
+      this._snackbar.snack("Error no announcement");
+    }
+  }
 }
