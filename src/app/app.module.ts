59d115809b559f39be20f108cffae86d63191cc7
callmemai <maelys.leberre@yahoo.com>
2023-05-30 15:06:18 +0200

    changes profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,6 +9,13 @@ import { HomeComponent } from './components/views/home/home.component';
 import { AuthComponent } from './components/views/auth/auth.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSortModule} from '@angular/material/sort';
+import {MatChipsModule} from '@angular/material/chips';
+import { MessageComponent } from './message/message.component';
+
+
 
 
 
@@ -18,13 +24,19 @@ import {MatCardModule} from '@angular/material/card';
     AppComponent,
     AuthComponent,
     HomeComponent,
-    ProfileComponent
+    ProfileComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
+    MatTabsModule,
+    MatIconModule,
+    MatSortModule,
+    MatChipsModule,
+  
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, multi: true, useClass: DateHttpInterceptor }
--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
+  
 })
 export class ProfileComponent {
 
