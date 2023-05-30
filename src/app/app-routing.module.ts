e7867db0b83f898ca64813b3e604e2cfddd93c54
Bejo62192 <130389920+Bejo62192@users.noreply.github.com>
2023-05-30 15:20:13 +0200

    changement création fichier

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './components/views/auth/auth.component';
 import { HomeComponent } from './components/views/home/home.component';
 
-
 const isLogged = () => !!localStorage.getItem("jwt");
 
 const routes: Routes = [
   {
     path: "auth", component: AuthComponent, canActivate: [() => !isLogged()],
   },
-  { path: "", component: HomeComponent}
+  { path: "home", component: HomeComponent},
 ];
 
 @NgModule({
