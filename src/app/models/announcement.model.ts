4f51bba0d84299dfde04ab960c217ed89936d4a0
Bejo62192 <130389920+Bejo62192@users.noreply.github.com>
2023-06-13 02:57:22 +0200

    Update announcement.model.ts
    Mise à jour du announcement.model pour suivre la bdd

--- src/app/models/announcement.model.ts
+++ src/app/models/announcement.model.ts
@@ -1,7 +1,33 @@
+enum AnimalEnum {
+    Cat,
+    Dog,
+    Frog,
+    Rabbit,
+    Turtle,
+    Fish,
+    Snake,
+    Bird,
+    Hamster,
+}
+
+enum HouseActivityEnum {
+    PET_SITTING,
+    PLANT_SITTING,
+    HOUSE_SITTING
+}
+
+enum HousingTypeEnum {
+    HOUSE,
+    APARTMENT,
+}
+
+
 export interface AnnouncementModel{
     id : number;
     address : string;
     city : string;
+    x: number
+    y: number
     postalcode : number;
     description : string;
     numberOfBeds : number;
@@ -10,5 +36,12 @@ export interface AnnouncementModel{
     stopDate : Date;
     numberPeopleMax : number;
     numberOfRooms : number;
+    activities: HouseActivityEnum;
+    housingType: HousingTypeEnum;
+    refusedAnimals: AnimalEnum;
+    allowedChildren: boolean;
+    allowedPets: boolean;
+    allowedSmoking: boolean;
+    wifi: boolean;
     imagePath: string;
 }
